e8f617fe6e15702f02e2bda79f6b1b5cef83703d
kbatrakov <kirillbatrakov0819@gmail.com>
2025-04-17 11:15:30 +0200

    Solution

--- app/cafe.py
+++ app/cafe.py
@@ -9,13 +9,15 @@ class Cafe:
 
     def visit_cafe(self, visitor: dict[str | str, int]) -> str:
         if "vaccine" not in visitor:
-            raise NotVaccinatedError
+            raise NotVaccinatedError("All friends should be vaccinated")
 
         today = datetime.date.today()
+
         if visitor["vaccine"]["expiration_date"] < today:
-            raise OutdatedVaccineError
+            raise OutdatedVaccineError("All friends should be vaccinated")
 
         if visitor["wearing_a_mask"] is False:
-            raise NotWearingMaskError
+            raise NotWearingMaskError("Friends should buy "
+                                      "{masks_to_buy} masks")
 
         return f"Welcome to {self.name}"
--- app/main.py
+++ app/main.py
@@ -5,16 +5,17 @@ from app.errors import VaccineError, NotWearingMaskError
 
 def go_to_cafe(friends: list, cafe: Cafe) -> str:
 
-    masks_to_buy = sum(not friend["wearing_a_mask"] for friend in friends)
+    masks_to_buy = 0
 
     for friend in friends:
         try:
             cafe.visit_cafe(friend)
         except VaccineError:
             return "All friends should be vaccinated"
-
         except NotWearingMaskError:
-            return f"Friends should buy {masks_to_buy} masks"
+            masks_to_buy += 1
 
-        else:
-            return f"Friends can go to {cafe.name}"
+    if masks_to_buy > 0:
+        return f"Friends should buy {masks_to_buy} masks"
+
+    return f"Friends can go to {cafe.name}"
